eb0b0c1e23395bdd0f5778937534a0396f9686aa
anas jabellah <jablcodiing@gmail.com>
2022-01-15 14:20:47 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+var arrowleftcircle = document.getElementById("arrowleftcircle");
+var contentright = document.getElementById("contentright");
+var navbar = document.getElementById("navbarleft");
+
+
+arrowleftcircle.addEventListener("click" , ()=> {
+    contentright.classList.toggle("right") ;
+    navbar.classList.toggle("left") ;
+})
+
+
+
+// function myFunction(){
+
+//     if () {
+        
+//     }
+
+// }
